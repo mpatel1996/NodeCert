c143a021e13e1e89b6fa66205b5fffcb95ff71f2
mpatel1996 <mpjv12343@gmail.com>
2022-07-08 22:16:27 -0500

    initial changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NodeCert
+---
+Creating simple repo to pack together all the things I learn for NodeJS to be ready for the Certification
---
+++ expres-web-app/app.js
@@ -0,0 +1,25 @@
+"use strict";
+const express = require("express");
+const createError = require('http-errors');
+const indexRoute = require('./routes');
+const helloRoute = require('./routes/hello');
+
+const app = express();
+
+app.use('/', indexRoute);
+app.use('/hello', helloRoute);
+
+app.use((req, res, next) => {
+    if (req.method !== "GET") {
+        next(createError(405));
+        return;
+    }
+    next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);  
+    res.send(err.message);
+});
+
+module.exports = app;
